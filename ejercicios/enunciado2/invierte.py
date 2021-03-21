1337c5a5e6eed45e87d20f8b550a594c485a4887
Nahuel Silva <nahuel_11_arg@hotmail.com>
2021-03-21 14:27:47 -0300

    actualizado

--- ejercicios/enunciado2/invierte.py
+++ ejercicios/enunciado2/invierte.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 import sys
-dato = sys.stdin.read()
-dato = dato[::-1]
-sys.stdout.write("Palabras invertidas:" + dato)
+
+a = sys.stdin.read()
+
+def invertir(cod):
+    return str(cod[::-1])
+
+sys.stdout.write("Palabras invertidas: " + invertir(a))
+
