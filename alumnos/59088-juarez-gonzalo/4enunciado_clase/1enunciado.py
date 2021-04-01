403b13278eef358735706432ecdd9a9fda3ff73f
juarez-gonza <gonza4266118@gmail.com>
2021-03-31 23:45:28 -0300

    1er enunciado de la tarea para la clase 4

---
+++ alumnos/59088-juarez-gonzalo/4enunciado_clase/1enunciado.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import os
+import sys
+import getopt
+
+import signal
+import time
+
+CMD = "ps"
+ARGS = ["-f"]
+SLEEPTIME = 3600
+
+def SIGUSR1_handler(signum, frame):
+    try:
+        while os.wait():
+            continue
+    except ChildProcessError as err:
+        if err.errno == 10:
+            os.execvp(CMD, ARGS)
+
+def usagendie():
+    print("Usage: %s -c <num>" % __file__)
+    sys.exit(1)
+
+if __name__ == "__main__":
+    try:
+        signal.signal(signal.SIGUSR1, SIGUSR1_handler)
+
+        opt, args = getopt.getopt(sys.argv[1:], "c:")
+        if len(opt) < 1 or len(opt[0]) < 1:
+            raise ValueError()
+        cnum = int(opt[0][1])
+
+        print(os.getpid())
+        for i in range(cnum):
+            if not os.fork():
+                sys.exit(666)
+
+        time.sleep(SLEEPTIME)
+    except ValueError as err:
+        usagendie()
