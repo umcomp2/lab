605720424a34d40c1952f5661bbaafdc41f6bf00
juarez-gonza <gonza4266118@gmail.com>
2021-04-01 11:36:52 -0300

    probando el signalizer

--- alumnos/59088-juarez-gonzalo/4enunciado_clase/1enunciado.py
+++ alumnos/59088-juarez-gonzalo/4enunciado_clase/1enunciado.py
@@ -12,9 +12,11 @@ SLEEPTIME = 3600
 
 def SIGUSR1_handler(signum, frame):
     try:
+        print("recv SIGUSR1")
         while os.wait():
             continue
     except ChildProcessError as err:
+        print("exception")
         if err.errno == 10:
             os.execvp(CMD, ARGS)
 
@@ -25,6 +27,7 @@ def usagendie():
 if __name__ == "__main__":
     try:
         signal.signal(signal.SIGUSR1, SIGUSR1_handler)
+        print(sys.argv)
 
         opt, args = getopt.getopt(sys.argv[1:], "c:")
         if len(opt) < 1 or len(opt[0]) < 1:
