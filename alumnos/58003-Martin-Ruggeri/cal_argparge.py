e9fee999610e66a883c15e0a72f906dc3d5bbf1b
Martin-Ruggeri-Bio <martinruggeri18@gmail.com>
2021-04-03 20:00:05 -0300

    mejorando calculadora argparse y copiando la exclusion mutua del profe

--- alumnos/58003-Martin-Ruggeri/cal_argparge.py
+++ alumnos/58003-Martin-Ruggeri/cal_argparge.py
@@ -7,16 +7,16 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-a', '--numero_a', type=int, help='Parámetro a')
 parser.add_argument('-b', '--numero_b', type=int, help='Parámetro b')
 parser.add_argument('-o', '--operacion', type=str,
-                    choices=['suma', 'resta', 'multiplicacion', 'division'],
+                    choices=['suma', 'resta', 'multi', 'divi'],
                     default=max, required=False,
-                    help='Operación a realizar con a y b')
+                    help='Las op para a y b son [suma, resta, multi, divi]')
 args = parser.parse_args()
 
 if args.operacion == 'suma':
     print(args.numero_a + args.numero_b)
 elif args.operacion == 'resta':
     print(args.numero_a - args.numero_b)
-elif args.operacion == 'multiplicacion':
+elif args.operacion == 'multi':
     print(args.numero_a * args.numero_b)
-elif args.operacion == 'division':
+elif args.operacion == 'divi':
     print(args.numero_a / args.numero_b)
