d6fc3b1facc326a7a7d2be0422989aff0d914e9e
AgnidDrage <marianosancheztoledo@mail.com>
2021-05-12 15:35:59 -0300

    avance

--- alumnos/59081-Sanchez-Toledo-Mariano/TP1-Recu/procesador.py
+++ alumnos/59081-Sanchez-Toledo-Mariano/TP1-Recu/procesador.py
@@ -4,13 +4,13 @@ import os
 import multiprocessing as mp
 import argparse
 import array
+import time
 
 
 def getHeader():
-    contador = 0
-    # Crea una lista con los datos del Header de la imagen.
-    fd = open(args.file, 'r')
+    #Crea una lista con los datos del Header de la imagen.
     header = []
+    fd = os.open(args.file, os.O_RDONLY)
     datos = os.read(fd, 50).split()
     for i in datos:
         if i == b'P6':
@@ -19,51 +19,53 @@ def getHeader():
             header.append(i)
         else:
             pass
-    os.close(fd)
     return header
 
+'''
+Ver como solucionar la lectura por bloques ya que no esta implementado...
+'''
+
+
+def getBody():
+    #* Obtiene el body
+    fd = open(args.file, 'rb').read()
+    finheader = fd.find(b"\n", fd.find(b"\n", fd.find(b"\n") + 1) + 1) + 1
+    body = fd[finheader:]
+    body = array.array('B', [i for i in body])
+    return body
+
+
+def splitBody(body):
+    #* Lee Body y lo separa en 3 Queues segun color.
+    pointer = -1
+    for i in body:
+        pointer += 1
+        if pointer % 3 == 0:
+            queueRed.put(i)
+        elif pointer % 3 == 1:
+            queueGreen.put(i)
+        elif pointer % 3 == 2:
+            queueBlue.put(i)
+        else:
+            break
 
 def rojo(header):
     rojo = []
-    pointer = 0
-    # Obtiene body y lo guarda en un array.
-    fd = open(args.file, 'rb')
-    data = fd.read()
-    finheader = data.find(b"\n", data.find(b"\n", data.find(b"\n") + 1) + 1) + 1
-    body = data[finheader:]
-    body = array.array('B', [i for i in body])
-    fd.close()
-
-    # Lee Body y guarda lo correspondiente al colo rojo.
-    for i in body:
-        if pointer % 3 == 0:
-            queueRed.put(i)
-        else:
-            pass
-        pointer += 1
-
-    # Comienza el rearmado de la imagen.
     fd = open('rojo.ppm', 'w')
-    fd.write('{}\n{} {}\n{}\n'.format(str(header[0]), str(
-        header[1]), str(header[2]), str(header[3])))
-
-    # Pasar cada valor de queue a lista correspondiente
+    fd.write('{}\n{} {}\n{}\n'.format(header[0], header[1], header[2], header[3]))
     while True:
+        #*Pasa cada valor de la Queue a la lista rojo.
         getQueue = queueRed.get()
-        rojo.append(getQueue)
+        rojo.append(getQueue) 
         if queueRed.qsize() == 0 or queueRed.qsize() is None:
             break
-
-    for i in rojo:
-        # Ver tema de la intensidad (esta omitido para probar)
-        pointer += 1
-        fd.write(i, '')
-        while range(0, 1):
-            fd.write(0, '')
-        if pointer == 4:
-            fd.write('\n')
-        else:
-            pass
+    #!BORRAR
+    print(rojo)
+    
+    '''for i in rojo:
+        fd.write(i)
+        fd.write(0)
+        fd.write(0)'''
 
 
 if __name__ == '__main__':
@@ -73,34 +75,23 @@ if __name__ == '__main__':
                          help='Indique cantidad de bytes por bloque')
     args = parser.parse_args()
 
-    try:
-        print('\n====Leyendo Archivo====\n')
-        header = getHeader()
-        queueRed = mp.Queue()
-        queueGreen = mp.Queue()
-        queueBlue = mp.Queue()
-        print('\nArchivo Leído Correctamente\n')
+    #try:
+    print('\n====Leyendo Archivo====\n')
+    header = getHeader()
+    print('Header obtenido...')
+    queueRed = mp.Queue()
+    queueGreen = mp.Queue()
+    queueBlue = mp.Queue()
+    print('Queues creadas...')
+    print('\nArchivo Leído Correctamente\n')
+    print('Iniciando recontruccion de imagen ppm...')
 
-    except:
-        print('\nError al leer Archivo, por favor verifica la ruta del archivo .ppm.\n')
-        exit(2)
+    #except:
+        #print('\nError al leer Archivo, por favor verifica la ruta del archivo .ppm.\n')
+        #exit(2)
 
-    proceRojo = mp.Process(target=rojo(header), args=(queueRed,))
-    #proceVerde = mp.Process(target=verde(), args=(queueGreen,))
-    #proceAzul = mp.Process(target=azul(), args=(queueBlue,))
-
+    proceRojo = mp.Process(target=rojo(header), args=(queueRed))
     proceRojo.start()
-    # proceVerde.start()
-    # proceAzul.start()
-
     proceRojo.join()
-    print('Proceso Rojo finalizo correctamente')
-    # proceVerde.join()
-    #print('Proceso Verde finalizo correctamente')
-    # proceAzul.join()
-    #print('Proceso Azul finalizo correctamente')
-
-    print('Error al generar procesos')
-    exit(2)
 
     exit(0)
