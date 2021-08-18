2f37a85b8fe66fe8bd233f09427c8afac5ba0dd0
juarez-gonza <gmjuareztello@gmail.com>
2021-08-18 16:24:46 -0300

    queued remote shell

--- alumnos/59088-juarez-gonzalo/9enunciado_clase/clase/remote_shell/test.py
+++ alumnos/59088-juarez-gonzalo/9enunciado_clase/clase/remote_shell/test.py
@@ -3,10 +3,19 @@ import subprocess as sp
 import socket
 import threading as tr
 
+# already synchronized (python3+)
+from queue import Queue
+
+NTHREADS = 64
+
 PORT = 8080
 HOSTNAME = ""
+BACKLOGSIZE = 5
 
 MAXINPUTSIZE = 1024
+
+tr_pool = [None]*NTHREADS
+conn_q = None
 
 def send_msg(s, msg):
     acc = 0
@@ -32,10 +41,6 @@ def recv_line(s):
 
     return line
 
-class Obj():
-    def __init__(self, returncode, stdout):
-        self.returncode = returncode
-        self.stdout = stdout
 #p = {
 #        returncode : int
 #        stdout : bytes
@@ -46,33 +51,49 @@ class Obj():
 # or stderr argument to Popen and indicates that a pipe to the
 # standard stream should be opened."
 def shell_loop(s):
-    print("Start of Thread %d with socket %s" % (tr.get_native_id(), str(s)))
-    try:
-        while cmd := recv_line(s):
-            p = sp.run(cmd, shell=True, stdout=sp.PIPE, stderr=sp.STDOUT)
-            #p = Obj(0, b"aaaa")
-            msg = bytearray()
-            if p.returncode != 0:
-                msg += bytes("ERROR. RETCODE %d" % p.returncode, "utf8")
-            else:
-                msg += bytes("OK. RETCODE: %d" % p.returncode, "utf8")
-            msg += b"\n\n" + p.stdout
-            send_msg(s, msg)
+    while cmd := recv_line(s):
+        p = sp.run(cmd, shell=True, stdout=sp.PIPE, stderr=sp.STDOUT)
+        msg = bytearray()
+        if p.returncode != 0:
+            msg += bytes("ERROR. RETCODE %d" % p.returncode, "utf8")
+        else:
+            msg += bytes("OK. RETCODE: %d" % p.returncode, "utf8")
+        msg += b"\n\n" + p.stdout
+        send_msg(s, msg)
 
-            if (cmd == b"exit"):
-                break
-    except EOFError:
-        pass
-    print("End of Thread %d, closing socket %s" % (tr.get_native_id(), str(s)))
+        if (cmd == b"exit"):
+            break
     s.close()
 
-if __name__ == "__main__":
+def pool_loop():
+    global conn_q
+
+    s, addr = conn_q.get()
+    shell_loop(s)
+
+
+def start_tr_pool():
+    for i in range(NTHREADS):
+        tr_pool.append(tr.Thread(target=pool_loop, args=()))
+        tr_pool[-1].start()
+
+def set_tcp_sv():
     sv_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sv_s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR | socket.SO_REUSEPORT, 1)
     sv_s.bind((HOSTNAME, PORT))
-    sv_s.listen(5)
+    sv_s.listen(BACKLOGSIZE)
+    return sv_s
+
+def main():
+    global conn_q
+
+    sv_s = set_tcp_sv()
+    conn_q = Queue()
+    start_tr_pool()
     while True:
-        s, _ = sv_s.accept()
-        new_t = tr.Thread(target=shell_loop,args=(s,))
-        new_t.start()
-        print("connection being served")
+        conn = sv_s.accept()
+        conn_q.put(conn)
+
+
+if __name__ == "__main__":
+    main()
