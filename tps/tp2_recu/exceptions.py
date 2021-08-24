701c1e1f2fb011e3852ba5b208cf25a39faf2f93
militomba <mariamilagrostomba@gmail.com>
2021-08-24 00:43:11 -0300

    tp2recuperatorio

--- tps/tp2_recu/exceptions.py
+++ tps/tp2_recu/exceptions.py
@@ -2,9 +2,7 @@ class NoPPMfile(Exception):
     """El archivo no es un archivo PPM"""
     pass
 
-class EscalaNoNegativa(Exception):
-    """La escala q otorgamos para cada color no puede ser negativa"""
-    pass
+
 
 class SizeNoNegativo(Exception):
     """Size no puede ser negativo"""
