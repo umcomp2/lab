c7840fe19b650e13c4898c5bdb1efb63589a8360
Taccetta <n.taccetta@alumno.um.edu.ar>
2021-08-24 02:32:44 -0300

    terminado

--- alumnos/57190-nicolas-taccetta/TP2/tp2.py
+++ alumnos/57190-nicolas-taccetta/TP2/tp2.py
@@ -6,10 +6,13 @@ import array
 import sys
 from concurrent.futures import ThreadPoolExecutor as TPE
 
+#================================================================================================================
+
 def image_reader(imagefile):
 
+    #Format Image ====================================
     text = f"Parent Process ID {os.getpid()} reading file..."
-    load_animation(text, 0.005)
+    load_animation(text, 0.05)
 
     global header
     global body
@@ -18,19 +21,19 @@ def image_reader(imagefile):
     global height
 
     try:
-        #Comments deleter
+        #Comments deleter ======================================
         for i in range(imagefile.count(b"\n# ")):
             coments1 = imagefile.find(b"\n# ")
             coments2 = imagefile.find(b"\n", coments1 + 1)
             imagefile = imagefile.replace(imagefile[coments1:coments2], b"")
         
-        #Formating image to int
+        #Formating image to int ==================================
         header_finder = imagefile.find(b"\n", imagefile.find(b"\n", imagefile.find(b"\n") +1) +1) +1
         header = imagefile[:header_finder].decode()
         body = imagefile[header_finder:]
         processimage = [i for i in body]
 
-        #rotacion header
+        #rotacion header =======================================
         space = header.find(' ')
         word1 = header.replace("P6\n", "")
         word1 = word1.replace(header[space:], "")
@@ -46,32 +49,28 @@ def image_reader(imagefile):
 
         print(width, height)
 
-        """header = header.replace(word1, word2)
-        header = header.replace(" "+word2, " "+word1)
-        print(header)"""
-
-        """def invertirMatriz():
-        for y in matrix:
-            y.reverse()
-        return matrix"""
-
-        #print(header[space:] + " " + header[:space])
-
         return processimage
 
     except:
         print("Error(1): Incorrect path or corrupt data file (must be bytes)")
         exit(3)
 
+# ================================================================================================================
+
 def creatergb(ppmread,rgb):
-    #Auxiliar variables
+
+    #Auxiliar variables ===============================
+    global finalimage
+    if finalimage == 0:
+        finalimage = [0] * len(ppmread)
     
     large = len(ppmread)
     g = -2
     b = -1
     r = 0
     filename = args.file.replace('.ppm', '')
-    #red creator
+
+    #red creator ===========================================================================================
     if rgb == 0:
         ppmr = ppmread.copy()
         position = 0
@@ -85,6 +84,7 @@ def creatergb(ppmread,rgb):
                 ppmr[b] = 0
         print("Red Processed")
 
+        #Mirror algorithm ================================
         candle.acquire()
         pixelsrgb = width * height * 3
         pixelwidth = width * 3
@@ -110,12 +110,25 @@ def creatergb(ppmread,rgb):
             pixel = pix + pixelpunt
 
         ppmr = listarev
+        #Creating .txt
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        printhisto = open(dir_path + '/' + histo_name + ".txt", "w")
+        printhisto.write(str(listarev))
+
+    #Applying mirror color in final image =========================
+        r = 0
+        for i in range(len(ppmr)//3):
+            finalimage[r] = ppmr[r]
+            r = r + 3
+            if r > len(ppmr):
+                break          
+
         candle.release()
         
         time.sleep(0.5)
         plot_image(ppmr, name)
 
-    #green creator
+    #green creator ===========================================================================================
     elif rgb == 1:
 
         ppmg = ppmread.copy()
@@ -130,11 +143,12 @@ def creatergb(ppmread,rgb):
                 ppmg[b] = 0
         print("Green processed")
 
-
+        #Mirror algorithm ==============================
         candle.acquire()
         pixelsrgb = width * height * 3
         pixelwidth = width * 3
 
+        listarev = [0] * pixelsrgb
         pixelsrgb = pixelsrgb - 1
         pixel = width * 3 - 1
         pixelpunt = pixel
@@ -152,12 +166,25 @@ def creatergb(ppmread,rgb):
 
             pixelcount = pixelcount + 1
             pixel = pix + pixelpunt 
-        ppmr = listarev   
+        ppmg = listarev
+        #Creating .txt
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        printhisto = open(dir_path + '/' + histo_name + ".txt", "w")
+        printhisto.write(str(listarev)) 
+
+        #Applying mirror color in final image ===============================
+        g = -2
+        for i in range(len(ppmg)//3):
+            finalimage[g] = ppmg[g]
+            g = g + 3
+            if g > len(ppmg):
+                break    
+
         candle.release()    
         
         plot_image(ppmg, name)
 
-    #blue creator
+    #blue creator==================================================================================================
     elif rgb == 2:
 
         ppmb = ppmread.copy()
@@ -172,35 +199,104 @@ def creatergb(ppmread,rgb):
                 ppmb[g] = 0
         print("Blue Processed")
 
+        #Mirror algorithm ==============================
+        candle.acquire()
+        
+        pixelsrgb = width * height * 3
+        pixelwidth = width * 3
+        listarev = [0] * pixelsrgb
+        pixelsrgb = pixelsrgb - 1
+        pixel = width * 3 - 1
+        pixelpunt = pixel
 
+        
+        #Mirror algorithm ====================================================
+        pix = 0
+        pixelcount = 1
+        for i in range(height):
+            
+            for j in range(width):
+                listarev[pix] = ppmb[pixel-2]
+                listarev[pix+1] = ppmb[pixel-1]
+                listarev[pix+2] = ppmb[pixel]
+                pix = pix + 3
+                pixel = pixel - 3
+
+            pixelcount = pixelcount + 1
+            pixel = pix + pixelpunt 
+        ppmb = listarev
+
+        #Creating .txt
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        printhisto = open(dir_path + '/' + histo_name + ".txt", "w")
+        printhisto.write(str(listarev))
+
+        #Applying mirror color in final image ====================================================
+        b = -1
+        for i in range(len(ppmb)//3):
+            finalimage[b] = ppmb[b]
+            b = b + 3
+            if b > len(ppmb):
+                break            
+
+        candle.release()   
 
         plot_image(ppmb, name)
 
     print("Filter created.")
-
-    #launch plot image
     
-    
+#launch plot image   
+#================================================================================================================
 
 def plot_image(ppmrgb, name):
     
     print("Launching plot image.")
 
-    #creating ppm filter
+    #creating ppm filter =============================================
     try:
         filter_rgb = array.array('B', [i for i in ppmrgb])
         with open(name + ".ppm", "wb", os.O_CREAT) as asing:
             asing.write(bytearray(header, 'ascii'))
             filter_rgb.tofile(asing)
             asing.close()
+            
+        print("PPM files whit RGB filters created.")
+
+    except:
+        print("Error: Can't create image.")
+        exit(2)
+
+#================================================================================================================
+
+def blackandwhite(ppmrgb, name):
     
+    print("Launching plot image Black and White Edition.")
+
+    jump = 0
+    for i in range(len(ppmrgb)//3):
+
+        ppmrgb[jump] = (ppmrgb[jump] + ppmrgb[jump+1] + ppmrgb[jump+2]) // 3
+        ppmrgb[jump+1] = ppmrgb[jump]
+        ppmrgb[jump+2] = ppmrgb[jump]
+
+        jump = jump + 3
+
+    #creating ppm filter Black and White ======================================
+    try:
+        filter_rgb = array.array('B', [i for i in ppmrgb])
+        with open(name + ".ppm", "wb", os.O_CREAT) as asing:
+            asing.write(bytearray(header, 'ascii'))
+            filter_rgb.tofile(asing)
+            asing.close()
+            
+        print("PPM files whit RGB filters created.")
+
     except:
         print("Error: Can't create image.")
         exit(2)
     
-    print("PPM files whit RGB filters created.")
 
-#Decorative animation        
+#Decorative animation ===========================================================================================        
 def load_animation(text, t):
     # String to be displayed when the application is loading
     load_str = text
@@ -247,18 +343,19 @@ def load_animation(text, t):
         print("\n")
         #os.system("clear")
 
-
+# =============================================================================================================
 if __name__ == "__main__":
     text1 = "Iniciating program"
 
     #Synchro
     candle = threading.Lock()
 
-    #Launch arguments
+    #Launch arguments =============================================
     try:
         parser = argparse.ArgumentParser(description='Processing PPM File')
         parser.add_argument('-f', '--file', help='File name')
         parser.add_argument('-s', '--size', type=int, help='Reading block size', default="1024")
+        parser.add_argument('-b', '--blackwhite', help='Black and White filter', default="0")
         args = parser.parse_args()
 
         if args.file == None:
@@ -274,7 +371,7 @@ if __name__ == "__main__":
         exit(2)
 
     load_animation(text1, 0.01)
-    #Open file and reading by blocks
+    #Open file and reading by blocks================== 
     try:
         ppm = os.open(args.file, os.O_RDONLY)
     except:
@@ -295,28 +392,27 @@ if __name__ == "__main__":
 
     print("Processing raw data...")
     time.sleep(0.5)
-    #Process raw data
+    #Process raw data ==================
     ppm_image = image_reader(ppm_raw)
     os.close(ppm)
     print("File ready...")
         
     print("Launch threading...")
     time.sleep(0.5)
+
     
+    global finalimage
+    finalimage = 0
+    
+    #Threading ==========================================================================================
     with TPE(max_workers=3) as executor: 
         executor.map(creatergb(ppm_image, 0))
         executor.map(creatergb(ppm_image, 1))
         executor.map(creatergb(ppm_image, 2))
 
-    """threads = list()
-    for i in range(3):
-        t = threading.Thread(target=creatergb(ppm_image, i))
-        threads.append(t)
-    
-    for t in threads:
-        t.start()
-
-    for t in threads:
-        t.join()"""
+    with TPE(max_workers=2) as executor: 
+        executor.map(plot_image(finalimage, "Mirror"))
+        if args.blackwhite == "y":
+            executor.map(blackandwhite(finalimage, "Mirror B&W"))
 
     exit(2)
