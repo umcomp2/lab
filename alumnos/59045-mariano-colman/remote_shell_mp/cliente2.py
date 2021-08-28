b189996c056faeb8b44a91c663ac8477514f7d3b
MarianoCol <marianocolman98@gmail.com>
2021-08-28 13:20:28 -0300

    Generando servidor multiclientes

---
+++ alumnos/59045-mariano-colman/remote_shell_mp/cliente2.py
@@ -0,0 +1,31 @@
+import socket as s
+import sys, os, subprocess
+
+
+sCliente = s.socket(s.AF_INET, s.SOCK_STREAM)
+
+host = sys.argv[1]
+
+port = int(sys.argv[2])
+
+sCliente.connect((host, port))
+
+pwd = sCliente.recv(1024)
+#msj = sCliente.recv(1024)
+#print("Hola cliente: {}".format(msj.decode()))
+
+print("[+] Trabajando en el directorio: ", pwd.decode())
+
+while True:
+    coman2 = input(f"\n{pwd.decode()} $> ")
+    if not coman2.strip():
+        continue
+    sCliente.send(coman2.encode())
+    if coman2.lower() == "exit":
+        break
+
+    output = sCliente.recv(2048).decode()
+
+    resultado, directorio = output.split("<sep>")
+
+    print(resultado)
---
+++ alumnos/59045-mariano-colman/remote_shell_mp/serverFork.py
@@ -0,0 +1,45 @@
+import socket as s
+import subprocess, os, sys, time
+
+server = s.socket(s.AF_INET, s.SOCK_STREAM)
+server.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
+
+SEPARATOR = "<sep>"
+print("[+] SERVIDOR INICIADO")
+host = s.gethostbyname(s.gethostname())
+port = int(sys.argv[1])
+
+print("\nHost: {} - Puerto: {}".format(host, port))
+
+server.bind((host, port))
+server.listen()
+
+while True:
+    socketCliente, addr = server.accept()
+    print("[+] CONEXION ACEPTADA DE {}:{}".format(addr[0], addr[1]))
+    directorio = os.getcwd()
+    socketCliente.send(directorio.encode())
+    cliente = os.fork()
+    if not cliente:
+        time.sleep(1)
+        msj = "Hola cliente n° {}".format(os.getpid())
+        socketCliente.send(msj.encode())
+        while True:
+            comando = socketCliente.recv(1024).decode()
+            if comando.lower() == "exit":
+                print("\n[+]CONEXION FINALIZADA DE {}:{}".format(addr[0], addr[1]))
+                break
+            else:
+                try:
+                    output = subprocess.getoutput(comando)
+                    rta = f"OK\n{output}{SEPARATOR}{directorio}"
+                except subprocess.CalledProcessError as error:
+                    rta = f"ERROR\n{output}{SEPARATOR}{directorio}"
+            socketCliente.send(rta.encode())
+        socketCliente.close()
+
+#s.close()
+    #comando = input(f"{msj} $> ")
+    #clienteSocket.send(comando.encode())
+    #if comando.lower() == "exit":
+    #    break
