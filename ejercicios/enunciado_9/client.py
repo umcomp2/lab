5e9699f6990d074d433b28045078a815fe1b2d92
IvanCsir <ivanfreiberg@gmail.com>
2021-08-31 09:13:48 -0300

    practica1

---
+++ ejercicios/enunciado_9/client.py
@@ -0,0 +1,30 @@
+import socket
+
+HEADER = 64
+PORT = 5086
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "127.0.1.1"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+
+while True:
+    comando = input("Ingrese un comando o exit:")
+    send(comando)
+    if comando == "exit":
+        send(DISCONNECT_MESSAGE)
+
+
+
