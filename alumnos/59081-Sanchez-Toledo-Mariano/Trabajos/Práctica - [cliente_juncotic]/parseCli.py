ced50abb7aaa222b5b34401e296539f038eb6205
AgnidDrage <marianosancheztoledo@gmail.com>
2021-08-31 20:51:00 -0300

    juncotic

---
+++ alumnos/59081-Sanchez-Toledo-Mariano/Trabajos/Práctica - [cliente_juncotic]/parseCli.py
@@ -0,0 +1,9 @@
+import argparse
+
+class Parser:
+    def parser():
+        parser = argparse.ArgumentParser(description='Cliente-servidor')
+        parser.add_argument('-i', '--ip', help='Ingrese host')
+        parser.add_argument('-p', '--port', type=int, help='Ingrese puerto')
+        args = parser.parse_args()
+        return args
