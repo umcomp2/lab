bc48513d61523c2811b1670642595c080f301296
MarianoSaez <mm.saez@alumno.um.edu.ar>
2021-09-04 12:57:02 -0300

    tcp_udp

--- alumnos/59161-mariano-saez/tareas/sockets/23_08_2021/tcp_udp/cliente.py
+++ alumnos/59161-mariano-saez/tareas/sockets/23_08_2021/tcp_udp/cliente.py
@@ -1,2 +1,62 @@
+import argparse
+import socket
+import os
+
+
+STDIN = 0
+
+def start(TYPE):
+    s = socket.socket(socket.AF_INET, TYPE)
+    return s
+
+def tcp(sock_cli, ADDR, PORT):
+    # Conectar al server
+    sock_cli.connect((ADDR, PORT))
+
+    while True:
+        data = os.read(STDIN, 4096)
+        sock_cli.send(data)
+        if data.decode() == "":
+            break
+        sock_cli.recv(4096)
+
+def udp(sock_cli, ADDR, PORT):
+    while True:
+        data = os.read(STDIN, 4096)
+        sock_cli.sendto(data, (ADDR, PORT))
+        if data.decode() == "":
+            break
+
+
+if __name__ == "__main__":
+
+    # Obtener por CLI [SERVER] y [PORT]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--address", type=str, default="127.0.0.1", help="Server IP")
+    parser.add_argument("-p", "--port", type=int, help="Server PORT")
+    parser.add_argument("-t", "--type", type=str, default="tcp", help="Protocolo de transporte")
+    args = parser.parse_args()
+
+    ADDR = args.address
+    PORT = args.port
+    if args.type == "tcp":
+        TYPE = socket.SOCK_STREAM
+    else:
+        TYPE = socket.SOCK_DGRAM
+
+    s = start(TYPE)
+    print("\nIniciando cliente con los siguiente parametros:")
+    print(f"\tDireccion destino: {ADDR}")
+    print(f"\tPuerto destino: {PORT}")
+    print(f"\tProtocolo: {args.type.upper()}\n")
+
+    if TYPE == "tcp":
+        tcp(s, ADDR, PORT)
+    else:
+        udp(s, ADDR, PORT)
+
+    print("Finalizando conexion...")
+    s.close()
+    exit(0)
 
 
--- alumnos/59161-mariano-saez/tareas/sockets/23_08_2021/tcp_udp/server.py
+++ alumnos/59161-mariano-saez/tareas/sockets/23_08_2021/tcp_udp/server.py
@@ -2,36 +2,83 @@ import socket
 import argparse as ap
 
 
-# Obtener los parametros por CLI
-parser = ap.ArgumentParser()
-parser.add_argument("-p", "--port", type=int,
-                    help="Puerto en el que atendera el server",)
-parser.add_argument("-t", "--type", type=str,
-                    help="Protocolo de capa de transporte TCP/UDP",
-                    default="tcp")
-parser.add_argument("-f", "--file", type=str,
-                    help="Ruta de archivo en el que escribir",
-                    default="/dev/null")
-args = parser.parse_args()
-
-PORT = args.PORT
-TYPE = args.type
-FILE = args.file
-
-
-def tcp():
+def start(proto, port, file):
     # Crear, bindear y poner a escuchar el server
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s = socket.socket(socket.AF_INET, proto)
     s.bind(("0.0.0.0", PORT))
-    s.listen()
+    print(f"Servidor iniciado en puerto {PORT}...")
 
     # Crear archivo (si no existe antes)
     f = open(f"{FILE}", "w+")
 
+    return s, f
+
+
+def tcp(s, f):
+    s.listen()
     conn, addr = s.accept()
+    print(f"Iniciando conexion con {addr}...")
 
     while True:
         data = conn.recv(4096).decode()
+        f.write(data)
+        f.flush()
+        if data == "":
+            print(f"Finalizando conexion con {addr}...")
+            conn.close()
+            break
+        conn.send(b"ACK") # Para habilitar a que envie nuevamente
+
+
+def udp(s, f):
+    while True:
+        data, addr = s.recvfrom(4096)
+        f.write(data.decode())
+        f.flush()
+
+        if data.decode() == "":
+            print(f"Finalizando conexion con {addr}...")
+            break
+
+
+
+if __name__ == "__main__":
+    # Obtener los parametros por CLI
+    parser = ap.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int,
+                        help="Puerto en el que atendera el server",)
+    parser.add_argument("-t", "--type", type=str,
+                        help="Protocolo de capa de transporte TCP/UDP",
+                        default="tcp")
+    parser.add_argument("-f", "--file", type=str,
+                        help="Ruta de archivo en el que escribir",
+                        default="/dev/null")
+    args = parser.parse_args()
+
+    PORT = args.port
+    FILE = args.file
+    if args.type == "tcp":
+        TYPE = socket.SOCK_STREAM
+    else:
+        TYPE = socket.SOCK_DGRAM
+
+    s, f = start(TYPE, PORT, FILE)
+    print("\nServidor iniciado con los siguiente parametros:")
+    print(f"\tPuerto: {PORT}")
+    print(f"\tProtocolo: {args.type.upper()}")
+    print(f"\tArchivo destino: {FILE}\n")
+
+    if TYPE == "tcp":
+        tcp(s,f)
+    else:
+        udp(s,f)
+
+    print("Cerrando servidor...")
+    f.close()
+    s.close()
+    exit(0)
+
+    
         
 
 
