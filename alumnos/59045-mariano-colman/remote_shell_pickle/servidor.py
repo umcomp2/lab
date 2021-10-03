0128accb61549332ecdadaad28d030233ce3e8f5
MarianoCol <marianocolman98@gmail.com>
2021-10-02 22:21:14 -0300

    Remote shell pickle

---
+++ alumnos/59045-mariano-colman/remote_shell_pickle/servidor.py
@@ -0,0 +1,42 @@
+import socket as s
+import threading, pickle, sys, os, time, subprocess, signal
+
+servidor = s.socket(s.AF_INET, s.SOCK_STREAM)
+servidor.getsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
+
+HOST = '0.0.0.0'
+PORT = int(sys.argv[1])
+SEPARATOR = "sep"
+
+servidor.bind((HOST, PORT))
+
+print("[+]SERVIDOR INICIADO!")
+
+servidor.listen()
+
+def consultas(directorio, socketCliente):
+    dir = pickle.dumps(directorio)
+    socketCliente.send(dir)
+    while True:
+        comando = socketCliente.recv(1024)
+        comando2 = pickle.loads(comando)
+        if comando2.lower() == "exit":
+            print("\n[+]CONEXION FINALIZADA DE {}:{}".format(addr[0], addr[1]))
+            break
+        else:
+            try:
+                output = subprocess.getoutput(comando2)
+                rta = f"OK\n{output}"
+            except subprocess.CalledProcessError as error:
+                rta = f"ERROR\n{output}"
+            socketCliente.send(pickle.dumps(rta))
+    socketCliente.close()
+
+signal.signal(signal.SIGCHLD, signal.SIG_IGN)
+while True:
+    socketCliente, addr = servidor.accept()
+    print("[+]CONEXION ESTABLECIDA DE {}:{}".format(addr[0], addr[1]))
+    directorio = os.getcwd()
+    time.sleep(1)
+    hilo = threading.Thread(target=consultas, args=(directorio, socketCliente,  ))
+    hilo.start()
