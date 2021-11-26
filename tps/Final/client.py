04087d1b86b7f07da30636e8d3c134e21e6a80bd
Nahuel Silva <nahuel_11_arg@hotmail.com>
2021-11-26 10:17:38 -0300

    actualizacion del dia

--- tps/Final/client.py
+++ tps/Final/client.py
@@ -1,12 +1,16 @@
 import argparse
 import socket
 import pickle
+import cv2
+import PIL
 
 parserito = argparse.ArgumentParser(description="Procesamiento de imagenes")
 
-parserito.add_argument("-i", "--ip", dest="ip", help="ip", type=str)
+parserito.add_argument("-i", "--ip", dest="ip", help="ip", type=str, required=True)
 parserito.add_argument("-p", "--puerto", dest="puerto",
                        help="Puerto", type=int, required=True)
+parserito.add_argument("-im", "--imagen", dest="imagen",
+                       help="Imagen a la que le quiere aplicar los cambios", type=str, required=True)
 parserito.add_argument("-e", "--edicio", dest="edicion",
                        help="Que edicion le quiere realizar a la imagen?", type=str, required=True)
 parserito.add_argument("-n1", "--n1", dest="n1", help="numero 1", type=int)
@@ -19,4 +23,25 @@ args = parserito.parse_args()
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 socket.connect((args.ip, args.puerto))
+
+msg = [args.imagen, args.edicion, args.n1, args.n2, args.n3, args.n4]
+serializer = pickle.dumps(msg)
+socket.sendall(serializer)
+datos = socket.recv(10000)
+datos_final = pickle.loads(datos) #Ver como se envia el return de las task
+if args.edicion == "imagen_borrosa":
+    cv2.imshow("borrosa_" + args.imagen, datos_final)
+    cv2.waitKeyEx(0)
+elif args.edicion == "bordes":
+    cv2.imshow("bordes_" + args.imagen, datos_final)
+    cv2.waitKeyEx(0)
+elif args.edicion == "enfocar":
+    cv2.imshow("enfocar" + args.imagen, datos_final)
+    cv2.waitKeyEx(0)
+else:
+    datos_final.show()
+
+print(">>>>>>>>>>Servidor desconectado<<<<<<<<<<")
+
+socket.close()
 
--- tps/Final/task.py
+++ tps/Final/task.py
@@ -8,62 +8,72 @@ def abrir_imagen(image):
 def escala_grises(image):
     imagen = abrir_imagen(image)
     imagen_gris = ImageOps.grayscale(imagen)
-    imagen_gris.show()
+    # imagen_gris.show()
     # imagen_gris.save("gris_"+ image)
+    return imagen_gris
 
 def invertir_colores(image):
     imagen = abrir_imagen(image)
     colores_inv = ImageChops.invert(imagen)
-    colores_inv.show()
+    # colores_inv.show()
     # colores_inv.save("coloresInv_"+ image)
+    return colores_inv
 
 def resaltar_luces(image, nro):
     imagen = abrir_imagen(image)
     #Entre mas grande es el enhance mas resalta las luces
     luces = ImageEnhance.Brightness(imagen).enhance(nro)
-    luces.show()
+    # luces.show()
+    return luces
 
 def contraste(image, nro):
     imagen = abrir_imagen(image)
     #Entre mas grande es el enhance mas contraste hace
     contraste = ImageEnhance.Contrast(imagen).enhance(nro)
-    contraste.show()
+    # contraste.show()
+    return contraste
 
 def espejado(image):
     imagen = abrir_imagen(image)
     espejada = ImageOps.mirror(imagen)
-    espejada.show()
+    # espejada.show()
+    return espejada
 
 def nitidez(image, nro):
     imagen = abrir_imagen(image)
     img_nit = ImageEnhance.Sharpness(imagen).enhance(nro)
-    img_nit.show()
+    # img_nit.show()
+    return img_nit
 
 def rotar_90(image):
     imagen = abrir_imagen(image)
     rotadaA90 = imagen.transpose(Image.ROTATE_90)
-    rotadaA90.show()
+    # rotadaA90.show()
+    return rotadaA90
 
 def rotar_270(image):
     imagen = abrir_imagen(image)
     rotadaA270 = imagen.transpose(Image.ROTATE_270)
-    rotadaA270.show()
+    # rotadaA270.show()
+    return rotadaA270
 
 def rotar_180(image):
     imagen = abrir_imagen(image)
     rotadaA180 = imagen.transpose(Image.ROTATE_180)
-    rotadaA180.show()
+    # rotadaA180.show()
+    return rotadaA180
 
 def tamaño(image, ancho, alto):
     imagen = abrir_imagen(image)
     ajuste = imagen.resize((ancho,alto))
-    ajuste.show()
+    # ajuste.show()
+    return ajuste
 
 def recortar(image, izq, sup, der, inf):
     imagen = abrir_imagen(image)
     recorte = imagen.crop((izq,sup,der,inf))
-    imagen.show()
-    recorte.show()
+    # recorte.show()
+    return recorte
 
 def unir_imagenes(image1, image2):
     imagen1 = abrir_imagen(image1)
@@ -73,28 +83,32 @@ def unir_imagenes(image1, image2):
         final = Image.new("RGB", (800,600), "black")
         final.paste(imagen1, (0,0))
         final.paste(imagen2, (400,0))
-        final.show()
+        # final.show()
+    return final
 
 def imagen_borrosa(image):
     img = cv2.imread(image)
     #La altura y el ancho (tupla) del kernel deben ser un numero positivo e impar
     img_borrosa = cv2.GaussianBlur(img, (11,11), 0)
-    cv2.imshow("borrosa_" + image, img_borrosa)
-    #Preciona Esc o alguna tecla para que cierre
-    cv2.waitKeyEx(0)
+    # cv2.imshow("borrosa_" + image, img_borrosa)
+    # Preciona Esc o alguna tecla para que cierre
+    # cv2.waitKeyEx(0)
     # cv2.imwrite("borroso_" + image , img_borrosa)
+    return img_borrosa
 
 def bordes(image):
     img = cv2.imread(image)
     edge_img = cv2.Canny(img, 100, 200)
-    cv2.imshow("bordes_" + image, edge_img)
-    cv2.waitKeyEx(0)
+    # cv2.imshow("bordes_" + image, edge_img)
+    # cv2.waitKeyEx(0)
+    return edge_img
 
 def enfocar(image):
     img = cv2.imread(image)
     resultado = cv2.fastNlMeansDenoisingColored(img, None, 15, 10, 7, 21)
-    cv2.imshow("enfoque_" + image, resultado)
-    cv2.waitKey(0)
+    # cv2.imshow("enfoque_" + image, resultado)
+    # cv2.waitKey(0)
+    return resultado
 
 def texto(image, texto):
     imagen = abrir_imagen(image)
@@ -103,4 +117,5 @@ def texto(image, texto):
     #(ancho, alto) --> en el alto entre mas chico es mas arriba va
     #              --> en el ancho entre mas grande es mas a la derecha va
     draw.text((30,10), texto, font=font, fill="white")
-    imagen.show()
+    # imagen.show()
+    return imagen
