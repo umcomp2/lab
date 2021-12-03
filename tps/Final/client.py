7945035939029fdc802fff15866996ed0b3e52fb
Nahuel Silva <nahuel_11_arg@hotmail.com>
2021-12-03 12:24:08 -0300

    me trabe

--- tps/Final/client.py
+++ tps/Final/client.py
@@ -1,4 +1,5 @@
 import argparse
+from os import truncate
 import socket
 import pickle
 import cv2
@@ -7,6 +8,7 @@ import json
 import base64
 from io import BytesIO
 import numpy as np
+import struct
 
 parserito = argparse.ArgumentParser(description="Procesamiento de imagenes")
 
@@ -21,6 +23,7 @@ parserito.add_argument("-n1", "--n1", dest="n1", help="numero 1", type=int)
 parserito.add_argument("-n2", "--n2", dest="n2", help="numero 2", type=int)
 parserito.add_argument("-n3", "--n3", dest="n3", help="numero 3", type=int)
 parserito.add_argument("-n4", "--n4", dest="n4", help="numero 4", type=int)
+parserito.add_argument("-t", "--texto", dest="texto", help="texto para agregar a la imagen")
 
 args = parserito.parse_args()
 
@@ -28,35 +31,40 @@ socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 socket.connect((args.ip, args.puerto))
 
-msg = [args.imagen, args.edicion, args.n1, args.n2, args.n3, args.n4]
+lista = [args.imagen, args.edicion, args.n1, args.n2, args.n3, args.n4, args.texto] 
+msg = []
+for i in lista:
+    if i != None:
+        msg.append(i)
 serializer = pickle.dumps(msg)
-socket.sendall(serializer)
-datos = socket.recv(10000)
-datos_final = pickle.loads(datos) 
-if args.edicion == "imagen_borrosa":
-    original_jpg = base64.b64decode(datos_final)
-    jpg_as_np = np.frombuffer(original_jpg, dtype=np.uint8)
-    imagen = cv2.imdecode(jpg_as_np, flags=1)
-    cv2.imshow("borrosa_" + args.imagen, imagen)
-    cv2.waitKeyEx(0)
-elif args.edicion == "bordes":
-    original_jpg = base64.b64decode(datos_final)
-    jpg_as_np = np.frombuffer(original_jpg, dtype=np.uint8)
-    imagen = cv2.imdecode(jpg_as_np, flags=1)
-    cv2.imshow("bordes_" + args.imagen, imagen)
-    cv2.waitKeyEx(0)
-elif args.edicion == "enfocar":
-    original_jpg = base64.b64decode(datos_final)
-    jpg_as_np = np.frombuffer(original_jpg, dtype=np.uint8)
-    imagen = cv2.imdecode(jpg_as_np, flags=1)
-    cv2.imshow("enfocar_" + args.imagen, imagen)
-    cv2.waitKeyEx(0)
-else:
-    img = json.dumps(datos_final)
-    img = base64.b64decode(img)
-    img = BytesIO(img)
-    img = Image.open(img)   
-    img.show()
+socket.send(serializer)
+# datos = str(socket.recv(50000), encoding="utf-8")
+# print(datos)
+
+# if args.edicion == "imagen_borrosa":
+#     original_jpg = base64.b64decode(datos)
+#     jpg_as_np = np.frombuffer(original_jpg, dtype=np.uint8)
+#     imagen = cv2.imdecode(jpg_as_np, flags=1)
+#     cv2.imshow("borrosa_" + args.imagen, imagen)
+#     cv2.waitKeyEx(0)
+# elif args.edicion == "bordes":
+#     original_jpg = base64.b64decode(datos)
+#     jpg_as_np = np.frombuffer(original_jpg, dtype=np.uint8)
+#     imagen = cv2.imdecode(jpg_as_np, flags=1)
+#     cv2.imshow("bordes_" + args.imagen, imagen)
+#     cv2.waitKeyEx(0)
+# elif args.edicion == "enfocar":
+#     original_jpg = base64.b64decode(datos)
+#     jpg_as_np = np.frombuffer(original_jpg, dtype=np.uint8)
+#     imagen = cv2.imdecode(jpg_as_np, flags=1)
+#     cv2.imshow("enfocar_" + args.imagen, imagen)
+#     cv2.waitKeyEx(0)
+# else:
+#     img = json.dumps(datos)
+#     img = base64.b64decode(img)
+#     img = BytesIO(img)
+#     img = Image.open(img)   
+#     img.show()
 
 print(">>>>>>>>>> Servidor desconectado <<<<<<<<<<")
 
--- tps/Final/server.py
+++ tps/Final/server.py
@@ -1,10 +1,12 @@
 import argparse
 import socketserver
 import pickle
+from tasks import *
+import queue
 
 parserito = argparse.ArgumentParser(description="Procesamiento de imagenes")
 
-parserito.add_argument("-i", "--ip", dest="ip", help="ip", type=str)
+parserito.add_argument("-i", "--ip", dest="ip", help="ip", required=True)
 parserito.add_argument("-p", "--puerto", dest="puerto",
                        help="Puerto", type=int, required=True)
 
@@ -13,27 +15,88 @@ args = parserito.parse_args()
 class MyTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         while True:
-            data = self.request.recv(10000)
-            if data == "exit":
+            data = self.request.recv(50000)
+            if not data:
                 break
             descerializado = pickle.loads(data)
-            if descerializado[1] == "resaltar_luces":
-                resultado = descerializado[1].delay(descerializado[0], descerializado[2])
-            elif descerializado[1] == "contraste":
-                resultado = descerializado[1].delay(descerializado[0], descerializado[2])
-            elif descerializado[1] == "nitidez":
-                resultado = descerializado[1].delay(descerializado[0], descerializado[2])
-            elif descerializado[1] == "tamaño":
-                resultado = descerializado[1].delay(descerializado[0], descerializado[2], descerializado[3])
-            elif descerializado[1] == "recortar":
-                resultado = descerializado[1].delay(descerializado[0], descerializado[2], descerializado[3], descerializado[4], descerializado[5])
-            elif descerializado[1] == "texto":
-                resultado = descerializado[1].delay(descerializado[0], descerializado[2])
-            else:
-                resultado = descerializado[1].delay(descerializado[0])
-        serializador = pickle.dumps(resultado.get())
-        self.request.sendall(serializador)
+            if descerializado[1] == "escala_grises":
+                resultado = escala_grises.delay(descerializado[0])
+                serializador = bytes(resultado.get(), encoding="utf-8")
+                print(serializador)
+                # self.request.sendall(serializador)
+            # elif descerializado[1] == "bordes":
+            #     resultado = bordes.delay(descerializado[0])
+            #     serializador = bytes(resultado.get(), "utf-8")
+            #     self.request.sendall(serializador)
 
+
+
+
+            # if descerializado[1] == "resaltar_luces":
+            #     resultado = resaltar_luces.delay(descerializado[0], descerializado[2])
+            #     serializador = bytes(resultado.get(), "utf-8")
+            #     self.request.sendall(serializador)
+            # elif descerializado[1] == "contraste":
+            #     resultado = contraste.delay(descerializado[0], descerializado[2])
+            #     serializador = bytes(resultado.get(), "utf-8")
+            #     self.request.sendall(serializador)
+            # elif descerializado[1] == "nitidez":
+            #     resultado = nitidez.delay(descerializado[0], descerializado[2])
+            #     serializador = bytes(resultado.get(), "utf-8")
+            #     self.request.sendall(serializador)
+            # elif descerializado[1] == "tamaño":
+            #     resultado = tamaño.delay(descerializado[0], descerializado[2], descerializado[3])
+            #     serializador = bytes(resultado.get(), "utf-8")
+            #     self.request.sendall(serializador)
+            # elif descerializado[1] == "recortar":
+            #     resultado = recortar.delay(descerializado[0], descerializado[2], descerializado[3], descerializado[4], descerializado[5])
+            #     serializador = bytes(resultado.get(), "utf-8")
+            #     self.request.sendall(serializador)
+            # elif descerializado[1] == "texto":
+            #     resultado = texto.delay(descerializado[0], descerializado[6])
+            #     serializador = bytes(resultado.get(), "utf-8")
+            #     self.request.sendall(serializador)
+            # elif descerializado[1] == "escala_grises":
+            #     resultado = escala_grises.delay(descerializado[0])
+            #     serializador = bytes(resultado.get(), "utf-8")
+            #     self.request.sendall(serializador)
+            # elif descerializado[1] == "invertir_colores":
+            #     resultado = invertir_colores.delay(descerializado[0])
+            #     serializador = bytes(resultado.get(), "utf-8")
+            #     self.request.sendall(serializador)
+            # elif descerializado[1] == "espejado":
+            #     resultado = espejado.delay(descerializado[0])
+            #     serializador = bytes(resultado.get(), "utf-8")
+            #     self.request.sendall(serializador)
+            # elif descerializado[1] == "rotar_90":
+            #     resultado = rotar_90.delay(descerializado[0])
+            #     serializador = bytes(resultado.get(), "utf-8")
+            #     self.request.sendall(serializador)
+            # elif descerializado[1] == "rotar_270":
+            #     resultado = rotar_270.delay(descerializado[0])
+            #     serializador = bytes(resultado.get(), "utf-8")
+            #     self.request.sendall(serializador)
+            # elif descerializado[1] == "rotar_180":
+            #     resultado = rotar_180.delay(descerializado[0])
+            #     serializador = bytes(resultado.get(), "utf-8")
+            #     self.request.sendall(serializador)
+            # elif descerializado[1] == "imagen_borrosa":
+            #     resultado = imagen_borrosa.delay(descerializado[0])
+            #     serializador = bytes(resultado.get(), "utf-8")
+            #     self.request.sendall(serializador)
+            # elif descerializado[1] == "bordes":
+            #     resultado = bordes.delay(descerializado[0])
+            #     serializador = bytes(resultado.get(), "utf-8")
+            #     self.request.sendall(serializador)
+            # elif descerializado[1] == "enfocar":
+            #     resultado = enfocar.delay(descerializado[0])
+            #     serializador = bytes(resultado.get(), "utf-8")
+            #     self.request.sendall(serializador)
+            # else:
+            #     print('No existe esa edicion!')
+            #     exit()
+
+    
 
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
