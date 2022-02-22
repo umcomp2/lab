b0c611d49d9b897a5dc04583e40c98fdea4d7a1f
Nahuel-Silva <nahuel_11_arg@hotmail.com>
2022-02-22 11:10:42 -0300

    algo nuevo

--- tps/Final/mongodb.py
+++ tps/Final/mongodb.py
@@ -1,4 +1,30 @@
 import pymongo
 from pymongo import MongoClient
+from csv import *
 
-client = MongoClient("localhost", 27017)
+class Pymongo():
+    def __init__(self):
+        self.client = MongoClient('mongodb://localhost:27017/')
+        self.db = self.client["images"]
+        self.collection1 = self.db["edits"]
+        self.collection2 = self.db["clients"]
+
+    
+    def see_inf(self):
+        
+        # Obtenemos la informacion de la coleccion ediciones
+        info = self.collection1.find()
+
+        # Obtenemos la informacion de la coleccion clientes
+        info = self.collection1.find()
+
+        # Obetenemos la informacion de la primera columna de cada fila
+        # keys = list(info.keys())
+
+        # Obetenemos la informacion de la segunda columna de cada fila
+        # values = list(info.values())
+
+
+
+    def save_inf(self):
+        pass
